89d9c2903569004b0b005c1810d5e202c2e96d23
Martin van der Werff <github@newinnovations.nl>
2025-06-08 12:59:47 +0200

    chore: restore windows subsystem

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#![windows_subsystem = "console"]
+#![windows_subsystem = "windows"]
 
 mod application;
 mod backends;
