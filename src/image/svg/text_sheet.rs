857536cad172596843a82c9cc7419459ea9c025a
Martin van der Werff <github@newinnovations.nl>
2025-09-13 22:59:06 +0200

    fix: removed obsolete windows directive

--- src/image/svg/text_sheet.rs
+++ src/image/svg/text_sheet.rs
@@ -17,7 +17,6 @@
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#[cfg(not(windows))]
 use std::path::Path;
 
 use resvg::usvg::{fontdb, Options, Tree};
