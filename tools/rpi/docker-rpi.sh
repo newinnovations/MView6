37bc8686e4b0e6f81d9a1b73d6263f38ed3f025d
Martin van der Werff <github@newinnovations.nl>
2025-10-07 18:14:25 +0200

    fix: removed interactive/tty docker options

--- tools/rpi/docker-rpi.sh
+++ tools/rpi/docker-rpi.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR=$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")
 cd ${SCRIPT_DIR}/../..
 
 # docker run --rm -ti -v .:/opt/mview6 rust:1-trixie /bin/bash
-docker run --rm -ti -v .:/opt/mview6 rust:1-trixie /bin/bash -c /opt/mview6/tools/rpi/build-cross-rpi.sh
+docker run --rm -v .:/opt/mview6 rust:1-trixie /bin/bash -c /opt/mview6/tools/rpi/build-cross-rpi.sh
